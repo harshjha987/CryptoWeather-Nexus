f330293ab5ac540183e7a78e87699d830a3a6055
harshjha987 <jha.harsh837@gmail.com>
2025-04-04 03:01:41 +0530

    trying to fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,18 +7,27 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
+  baseDirectory:  __dirname,
 });
+// const eslintConfig = [
+//   ...compat.config({
+//     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+//     plugins: ["prettier"],
+//     rules: {
+//       "react/no-unescaped-entities": "off",
+//       "@next/next/no-page-custom-font": "off",
+//       "prettier/prettier": "error",
+//     },
+//   }),
+// ];
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
-    plugins: ["prettier"],
+    extends: ['next'],
     rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
-      "prettier/prettier": "error",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   }),
-];
+]
 
 export default eslintConfig;
